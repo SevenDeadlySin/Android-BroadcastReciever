91565aee0d7af51fd862e6aaeab4379a0142f180
BamBo <raksameth@gmail.com>
2017-06-07 21:27:22 +0700

    Implement BroadcastReciever
    Add BroadcastReciever : (MyBroadcastReciver)

---
+++ Getting/app/src/main/java/com/example/raksa/broadcastrecieverbasic/MyBroadcastReciever.java
@@ -0,0 +1,16 @@
+package com.example.raksa.broadcastrecieverbasic;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Raksa on 6/7/2017.
+ */
+
+public class MyBroadcastReciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
