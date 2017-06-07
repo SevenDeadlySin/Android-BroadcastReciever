3678e6c6d0d5dea44c56e1d22aee2ccdb738c2bc
BamBo <raksameth@gmail.com>
2017-06-07 21:34:58 +0700

    Add Code Block To onReciever Method
    Add Code Block to onReciever Method in MyBroadcastReciever Class

--- Getting/app/src/main/java/com/example/raksa/broadcastrecieverbasic/MyBroadcastReciever.java
+++ Getting/app/src/main/java/com/example/raksa/broadcastrecieverbasic/MyBroadcastReciever.java
@@ -3,14 +3,23 @@ package com.example.raksa.broadcastrecieverbasic;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Raksa on 6/7/2017.
  */
 
 public class MyBroadcastReciever extends BroadcastReceiver {
+
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Log.i("Reciever Info Message :"," A Message was Deliveried to MyBroadcastReciever");
+        Toast.makeText(context,"The Message was deliveried from The Broadcast!!",Toast.LENGTH_SHORT).show();
+        
 
     }
 }
