5e322a63e5517fb915894291cf4554f2b8ba187d
BamBo <raksameth@gmail.com>
2017-06-20 20:02:38 +0700

    Add Three Different BroadcastReciever
    Created Three Different BroadcastReciever For Ordered Broadcast Testing

--- OrderedBroadcastReciever/app/src/main/java/com/raksa/orderedbroadcastreciever/Activity/MainActivity.java
+++ OrderedBroadcastReciever/app/src/main/java/com/raksa/orderedbroadcastreciever/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.raksa.orderedbroadcastreciever;
+package com.raksa.orderedbroadcastreciever.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.raksa.orderedbroadcastreciever.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ OrderedBroadcastReciever/app/src/main/java/com/raksa/orderedbroadcastreciever/Broadcast/FirstReciever.java
@@ -0,0 +1,16 @@
+package com.raksa.orderedbroadcastreciever.Broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by User on 6/20/2017.
+ */
+
+public class FirstReciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
