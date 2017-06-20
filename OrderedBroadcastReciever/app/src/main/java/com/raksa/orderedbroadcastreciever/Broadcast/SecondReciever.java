252b1e943aad4bae79e47a329e6fc8ecd7a71a5d
BamBo <raksameth@gmail.com>
2017-06-20 20:37:01 +0700

    Add CodeBlock For onReciever Method of Each Reciever

--- OrderedBroadcastReciever/app/src/main/java/com/raksa/orderedbroadcastreciever/Broadcast/SecondReciever.java
+++ OrderedBroadcastReciever/app/src/main/java/com/raksa/orderedbroadcastreciever/Broadcast/SecondReciever.java
@@ -3,6 +3,9 @@ package com.raksa.orderedbroadcastreciever.Broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
+
+import com.raksa.orderedbroadcastreciever.Activity.MainActivity;
 
 /**
  * Created by User on 6/20/2017.
@@ -11,6 +14,6 @@ import android.content.Intent;
 public class SecondReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context,"The Data was sent to SecondReciever !",Toast.LENGTH_SHORT).show();
     }
 }
--- OrderedBroadcastReciever/app/src/main/java/com/raksa/orderedbroadcastreciever/Broadcast/ThirdReciever.java
+++ OrderedBroadcastReciever/app/src/main/java/com/raksa/orderedbroadcastreciever/Broadcast/ThirdReciever.java
@@ -3,6 +3,7 @@ package com.raksa.orderedbroadcastreciever.Broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by User on 6/20/2017.
@@ -11,6 +12,6 @@ import android.content.Intent;
 public class ThirdReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context,"The Data was sent to ThirdReciever !",Toast.LENGTH_SHORT).show();
     }
 }
